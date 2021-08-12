2b673c82d0bcaf264593ece07ff57e48c01f6d23
Thy-suntheng <thysuntheng2000@gmail.com>
2021-08-12 10:32:35 +0700

    Update

---
+++ App.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.title}>
+        The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
+      </Text>
+      <Button
+        title="Press me"
+        color="#34eb5c"
+        onPress={() => Alert.alert('Hello Thy suntheng')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
+      </Text>
+      <Button
+        title="theng"
+        color="#f194ff"
+        onPress={() => Alert.alert('Welcome to Cambodia')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        All interaction for the component are disabled.
+      </Text>
+      <Button
+        title="touch me"
+        color="#25d9d3"
+        onPress={() => Alert.alert('Hello from Cambodia')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        This layout strategy lets the title define the width of the button.
+      </Text>
+      <View style={styles.fixToText}>
+        <Button
+          title="Left button" 
+          color="#d51ddb"
+          onPress={() => Alert.alert('Left button pressed')}
+        />
+        <Button
+          title="Right button"
+          color="#34d90b"
+          onPress={() => Alert.alert('Right button pressed')}
+        />
+      </View>
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"#d90f52",
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 20,
+    borderBottomColor: '#eb4034',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default App;
